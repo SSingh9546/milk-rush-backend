7f9070098ca13bbbfee7e0c85eb3603946186405
Sudhanshu Singh <sudhanshu@silofortune.com>
2025-08-28 02:33:14 +0530

    farm-phone-added

--- src/services/fdo/fdoDataService.js
+++ src/services/fdo/fdoDataService.js
@@ -148,7 +148,7 @@ exports.getSpecificFdoData = async (fdoId, is_new) => {
   const [farmerData, farmDetails] = await Promise.all([
     FarmerData.findAll({
       where: { farm_id: { [Op.in]: farmIds } },
-      attributes: ['farm_id', 'farm_name']
+      attributes: ['farm_id', 'farm_name', 'phone']
     }),
     FarmDetails.findAll({
       where: { farm_id: { [Op.in]: farmIds } },
@@ -157,7 +157,7 @@ exports.getSpecificFdoData = async (fdoId, is_new) => {
   ]);
   
   const farmMap = {};
-  farmerData.forEach(farm => farmMap[farm.farm_id] = { farm_name: farm.farm_name });
+  farmerData.forEach(farm => farmMap[farm.farm_id] = { farm_name: farm.farm_name, farm_phone:farm.phone });
   farmDetails.forEach(farm => {
     if (farmMap[farm.farm_id]) {
       farmMap[farm.farm_id] = {
